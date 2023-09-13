59a5aba9a91a791cc7ae879ed84060115693f934
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2023-09-13 00:00:10 -0600

    a

--- update_images.sh
+++ update_images.sh
@@ -3,6 +3,6 @@
 CATEGORY=$1
 TOTAL_ITERATIONS=$2
 
-#python selenium_controler.py $CATEGORY $TOTAL_ITERATIONS
+python selenium_controler.py $CATEGORY $TOTAL_ITERATIONS
 python cleaning_html.py $CATEGORY
 python download_images.py $CATEGORY
